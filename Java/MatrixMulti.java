e46bfde1c60a41eaae00ba301e1f39ced3403ab8
Juan Vera <jlverarosas@cpp.edu>
2020-05-04 12:24:21 -0700

    Added Memory Usage and fixed passing methods

--- Java/MatrixMulti.java
+++ Java/MatrixMulti.java
@@ -16,37 +16,39 @@ public class MatrixMulti {
   }
 
   private static void passByReference() {
-    int matrixLength = 2;
+    int matrixLength = 200;
 
     while (true) {
       int[][] matrix1 = generateNewMatrix(matrixLength);
       int[][] matrix2 = generateNewMatrix(matrixLength);
+	  int[][] matrix3 = new int[matrixLength][matrixLength];
+	  
       final long startTime = System.currentTimeMillis();
-      runAlgorithm(matrix1, matrix2);
+      reference(matrix1, matrix2, matrix3);
       final long endTime = System.currentTimeMillis();
       System.out.println("[passByReference] Total execution time (Matrix Size = " + matrixLength
           + " x " + matrixLength + "): " + (endTime - startTime));
-      matrixLength *= 2;
+		System.out.println("KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
+      matrixLength += 200;
     }
   }
 
   private static void passByValue() {
-    int matrixLength = 2;
-    int[][] matrix1 = generateNewMatrix(matrixLength);
-    int[][] matrix2 = generateNewMatrix(matrixLength);
-    final long startTime = System.currentTimeMillis();
-    int[][] result = new int[matrix1.length][matrix1.length];
-    for (int i = 0; i < matrix1.length; i++) {
-      for (int j = 0; j < matrix1.length; j++) {
-        for (int k = 0; k < matrix1.length; k++) {
-          result[i][j] += matrix1[i][k] * matrix2[k][j];
-        }
-      }
-    }
-    final long endTime = System.currentTimeMillis();
+    int matrixLength = 200;
 
-    System.out.println("[passByValue] Total execution time (Matrix Size = " + matrixLength
-        + " x " + matrixLength + "): " + (endTime - startTime));
+    while (true) {
+      int[][] matrix1 = generateNewMatrix(matrixLength);
+      int[][] matrix2 = generateNewMatrix(matrixLength);
+	  int[][] matrix3 = new int[matrixLength][matrixLength];
+	  
+      final long startTime = System.currentTimeMillis();
+      value(matrix1, matrix2, matrix3);
+      final long endTime = System.currentTimeMillis();
+      System.out.println("[passByValue] Total execution time (Matrix Size = " + matrixLength
+          + " x " + matrixLength + "): " + (endTime - startTime));
+      System.out.println("KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
+      matrixLength += 200;
+    }
   }
 
   private static int[][] generateNewMatrix(int n) {
@@ -60,14 +62,39 @@ public class MatrixMulti {
     return newMatrix;
   }
 
-  private static void runAlgorithm(int[][] matrix1, int[][] matrix2) {
-    int[][] result = new int[matrix1.length][matrix1.length];
+  
+  
+  private static void reference(int[][] matrix1, int[][] matrix2, int[][] matrix3) {
     for (int i = 0; i < matrix1.length; i++) {
       for (int j = 0; j < matrix1.length; j++) {
         for (int k = 0; k < matrix1.length; k++) {
-          result[i][j] += matrix1[i][k] * matrix2[k][j];
+          matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
         }
       }
     }
+  }
+  
+   private static void value(int[][] matrix1, int[][] matrix2, int[][] matrix3) {
+	int[][] m1 = copyOf(matrix1);
+	int[][] m2 = copyOf(matrix2);
+	int[][] m3 = copyOf(matrix3);
+	
+    for (int i = 0; i < matrix1.length; i++) {
+      for (int j = 0; j < matrix1.length; j++) {
+        for (int k = 0; k < matrix1.length; k++) {
+          m3[i][j] += m1[i][k] * m2[k][j];
+        }
+      }
+    }
+  }
+  
+  private static int[][] copyOf(int[][] m) {
+	  int[][] toReturn = new int[m.length][m.length];
+	  for (int i = 0; i < m.length; i++) {
+		for (int j = 0; j < m.length; j++) {
+			toReturn[i][j] = m[i][j];
+		}
+	}
+	return toReturn;
   }
 }
