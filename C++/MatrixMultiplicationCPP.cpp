5393fb4b7ab3e3b04f9fde5f0522f613f744941b
Juan Vera <jlverarosas@cpp.edu>
2020-05-04 18:09:16 -0700

    Fixed passing by value

--- C++/MatrixMultiplicationCPP.cpp
+++ C++/MatrixMultiplicationCPP.cpp
@@ -1,95 +1,96 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
+#include <psapi.h>
+#define size 1200
 
 using namespace std;
 
-int main() {
-	//Declaring variables for rows/columns and initializing the matrices
-	int rowA, columnA, rowB, columnB;
-	int a[100][100];
-	int b[100][100];
-	int c[100][100];
-    
-	//Ensures the randomly generated values won't be repeated
-	srand(time(NULL));
-    
-	//Takes input from user to set matrix sizes
-	cout << "# of rows for matrix A: ";
-	cin >> rowA;
-	cout << "# of columns for matrix A: ";
-	cin >> columnA;
-	cout << "# of rows for matrix B: ";
-	cin >> rowB;
-	cout << "# of columns for matrix B: ";
-	cin >> columnB;
-	cout << endl;
+struct Matrix {
+	int** arr = new int*[size]; 
+};
 
-	// Checks if matrix A and B cannot be multiplied
-	while (columnA != rowB) {
-		cout << "Incompatible matrix sizes!";
-	}
-    
-	//Starts a timer to measure execution time
-	clock_t begin = clock();
-
-	//Fills Matrix A with random numbers between 1-100
-	for (int i = 0; i < rowA; i++) {
-		for (int j = 0; j < columnA; j++) {
-			a[i][j] = rand() % 100 + 1;
-		}
-	}
-    
-	//Fills Matrix B with random numbers between 1-100
-	for (int i = 0; i < rowB; i++) {
-		for (int j = 0; j < columnB; j++) {
-			b[i][j] = rand() % 100 + 1;
-		}
-	}
-
-	//Prints out Matrix A
-	cout << "Matrix A: " << endl;
-	for (int i = 0; i < rowA; i++) {
-		for (int j = 0; j < columnA; j++) {
-			cout << a[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	
-	//Prints out Matrix B
-	cout << "Matrix B: " << endl;
-	for (int i = 0; i < rowB; i++) {
-		for (int j = 0; j < columnB; j++) {
-			cout << b[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	
+void reference(struct Matrix &a, struct Matrix &b, struct Matrix &c) {
 	//Performs matrix multiplication for matrix A and B
-	for (int i = 0; i < rowA; i++) {
-		for (int j = 0; j < columnB; j++) {
-			c[i][j] = 0;
-			for (int k = 0; k < rowB; k++) {
-				c[i][j] += a[i][k] * b[k][j];
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			for (int k = 0; k < size; k++) {
+				c.arr[i][j] += a.arr[i][k] * b.arr[k][j];
 			}
 		}
 	}
-    
-	//Prints out Matrix C
-	cout << "Resulting Matrix: " << endl;
-	for (int i = 0; i < rowA; i++) {
-		for (int j = 0; j < columnB; j++) {
-			cout << c[i][j] << "\t";
+}
+
+void value(struct Matrix a, struct Matrix b, struct Matrix c) {
+	//Performs matrix multiplication for matrix A and B
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			for (int k = 0; k < size; k++) {
+				c.arr[i][j] += a.arr[i][k] * b.arr[k][j];
+			}
 		}
-		cout << endl;
 	}
+}
+
+int main() {
+
+	int choice = -1;
 	
-	//Stops the timer and returns the execution time in milliseconds
-	cout << endl;
-	clock_t end = clock();
-	double clockTicks = double(end - begin);
-	double timeTaken_ms = clockTicks / (CLOCKS_PER_SEC / 1000);
-	cout << "Execution Time: " << timeTaken_ms << " ms";
+	cout << "0 for value, 1 for reference: ";
+	cin >> choice;
+	
+	struct Matrix a;
+	struct Matrix b;
+	struct Matrix c;
+	
+	for(int i = 0; i < size; i++) {
+		a.arr[i] = new int[size];
+		b.arr[i] = new int[size];
+		c.arr[i] = new int[size];
+	}
+	//Ensures the randomly generated values won't be repeated
+	srand(time(NULL));
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				a.arr[i][j] = rand() % 100 + 1;
+				b.arr[i][j] = rand() % 100 + 1;
+				c.arr[i][j] = 0;
+			}
+		}
+	/*						
+	SIZE_T memory;
+	auto myHandle = GetCurrentProcess();
+	PROCESS_MEMORY_COUNTERS pmc;
+	if (GetProcessMemoryInfo(myHandle, &pmc, sizeof(pmc)))
+		memory =  pmc.WorkingSetSize;
+	else
+		cout << "error";
+	*/			
+		//Starts a timer to measure execution time
+		clock_t begin = clock();
+
+		if(choice == 0) {
+			value(a,b,c);
+		}
+		else {
+			reference(a,b,c);
+		}
+		
+		//Stops the timer and returns the execution time in milliseconds
+		cout << endl;
+		clock_t end = clock();
+		double clockTicks = double(end - begin);
+		double timeTaken_ms = clockTicks / (CLOCKS_PER_SEC / 1000);
+		cout << "Starting " << (choice == 0 ? "value ":"reference ") << "Size : " << size << " execution Time: " << timeTaken_ms << " ms";
+		cout << endl;
+	/*
+	myHandle = GetCurrentProcess();
+	if (GetProcessMemoryInfo(myHandle, &pmc, sizeof(pmc)))
+		cout << pmc.WorkingSetSize - memory;
+	else
+		cout << "error";
+    */
+
 }
